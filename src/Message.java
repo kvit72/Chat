8c1e6978ec4055d4523452a143456eda920bcc14
kvit7 <kvit72v@yandex.ru>
2023-03-29 11:35:42 +0300

    Написание класса Message.

--- src/Message.java
+++ src/Message.java
@@ -1,2 +1,24 @@
-public class Message {
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    private final MessageType type;
+    private final String data;
+
+    public Message(MessageType type) {
+        this.type = type;
+        this.data = null;
+    }
+
+    public Message(MessageType type, String data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
 }

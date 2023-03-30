48c11208df84fb3205af51da7b3710ae16f043bc
kvit7 <kvit72v@yandex.ru>
2023-03-30 17:32:11 +0300

    Создание пакета client и класса Client.

---
+++ src/client/Client.java
@@ -0,0 +1,12 @@
+package client;
+
+import java.sql.Connection;
+
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected;
+
+    public class SocketThread extends Thread {
+
+    }
+}

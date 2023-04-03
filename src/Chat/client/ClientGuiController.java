907ee1d3bab850d6b9f86db79f38e6135a268533
kvit7 <kvit72v@yandex.ru>
2023-04-03 19:08:21 +0300

    Создан класс ClientGuiController реализующий Control (MVC).

--- src/Chat/client/ClientGuiController.java
+++ src/Chat/client/ClientGuiController.java
@@ -1,4 +1,71 @@
 package Chat.client;
 
-public class ClientGuiController {
+public class ClientGuiController extends Client {
+    private ClientGuiModel model = new ClientGuiModel();
+    private ClientGuiView view = new ClientGuiView(this);
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new GuiSocketThread();
+    }
+
+    public ClientGuiModel getModel() {
+        return model;
+    }
+
+    @Override
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        socketThread.run();
+    }
+
+    public static void main(String[] args) {
+        ClientGuiController client = new ClientGuiController();
+        client.run();
+    }
+
+    @Override
+    protected String getServerAddress() {
+        return view.getServerAddress();
+    }
+
+    @Override
+    protected int getServerPort() {
+        return view.getServerPort();
+    }
+
+    @Override
+    protected String getUserName() {
+        return view.getUserName();
+    }
+
+    @Override
+    protected void sendTextMessage(String text) {
+        super.sendTextMessage(text);
+    }
+
+    public class GuiSocketThread extends SocketThread {
+        @Override
+        protected void processIncomingMessage(String message) {
+            // Выводим текст сообщения
+            model.setNewMessage(message);
+            view.refreshMessages();
+        }
+
+        protected void informAboutNewUser(String userName) {
+            // Выводим информацию о добавлении нового пользователя
+            model.addUser(userName);
+            view.refreshUsers();
+        }
+
+        protected void informAboutDeletingNewUser(String userName) {
+            // Выводим актуальный список пользователей
+            model.deleteUser(userName);
+            view.refreshUsers();
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            view.notifyConnectionStatusChanged(clientConnected);
+        }
+    }
 }
